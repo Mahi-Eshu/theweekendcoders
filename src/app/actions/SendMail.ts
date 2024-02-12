3b76a659206cafdf7a9a3eb9c8ec4dd51294cea4
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2024-02-12 18:53:40 +0530

    Second Commit

--- src/app/actions/SendMail.ts
+++ src/app/actions/SendMail.ts
@@ -3,7 +3,7 @@
 import React from 'react'
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_iycyNyB1_GBhPXSQAdDeFLE7k5xkdurch");
 
 
 export const SendMail = async (formdata: FormData) => {
