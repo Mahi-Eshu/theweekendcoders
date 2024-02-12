dee4007d5b85298838b88ef1d715a9640130bca4
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2024-02-12 19:02:53 +0530

    Fourth Commit

--- src/app/actions/SendMail.ts
+++ src/app/actions/SendMail.ts
@@ -1,10 +1,6 @@
 "use server"
 
 import React from 'react'
-import { Resend } from 'resend';
-
-const resend = new Resend("re_PhpFCqdX_9PMfd5Q6MNrR8352KENoHBwa");
-
 
 export const SendMail = async (formdata: FormData) => {
     console.log("This is send Mail Function")
@@ -15,19 +11,6 @@ export const SendMail = async (formdata: FormData) => {
     const requirement = formdata.get('requirement') as string;
     const description = formdata.get('description') as string;
     
-
-   try{
-    const {data, error} = await resend.emails.send({
-        from: 'theweekendcodershq@gmail.com',
-        to: 'mukindramgs04@gmail.com',
-        subject: 'Message From Resend',
-        text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nProfession: ${profession}\nRequirement`
-        })
-    console.log(data);
-    console.log(error);
-   }catch(err){
-    console.log(err)
-   }
 
 }
 
