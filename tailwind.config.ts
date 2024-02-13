0e4b0254c0f465199ab4f95e58c9b22eca2df945
AMARMG04 <117839448+AMARMG04@users.noreply.github.com>
2024-02-13 13:40:01 +0530

    15th Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-pattern": "url('/images/BG10.jpg')",
+        "hero-pattern": "url('/images/BG10.jpeg')",
       },
     },
   },
